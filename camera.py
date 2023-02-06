c43649333daa6c8f0d960a0516f4d5081a1f3ca6
Rahul Prasad <programmerahul@gmail.com>
2023-02-06 23:38:49 +0530

    first commit

---
+++ camera.py
@@ -0,0 +1,41 @@
+import glm
+import pygame as pg 
+FOV = 50
+NEAR=0.1
+FAR=100
+SPEED =0.01
+
+class Camera:
+    def __init__(self,app):
+        self.app=app
+        self.aspect_ratio = app.WIN_SIZE[0]/app.WIN_SIZE[1]
+        self.position= glm.vec3(0,2,10)
+        self.up= glm.vec3(0,1,0)
+        self.right=glm.vec3(1,0,0)
+        self.forward=glm.vec3(0,0,-1)
+        #view matrix
+        self.m_view=self.get_view_matrix()
+        #projection matrix:
+        self.m_proj=self.get_projection_matrix()
+    def update(self):
+        self.move()
+        self.m_view=self.get_view_matrix()
+    def move(self):
+        velocity=SPEED*self.app.delta_time
+        keys=pg.key.get_pressed()
+        if(keys[pg.K_w]):
+            self.position+=self.forward*velocity
+        if(keys[pg.K_s]):
+            self.position-=self.forward*velocity
+        if(keys[pg.K_a]):
+            self.position-=self.right*velocity
+        if(keys[pg.K_d]):
+            self.position+=self.right*velocity
+        if(keys[pg.K_q]):
+            self.position+=self.up*velocity
+        if(keys[pg.K_e]):
+            self.position-=self.up*velocity
+    def get_view_matrix(self):
+        return glm.lookAt(self.position,self.position+self.forward,self.up)
+    def get_projection_matrix(self):
+        return glm.perspective(glm.radians(FOV),self.aspect_ratio,NEAR,FAR)
